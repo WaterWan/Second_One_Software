ab819573d3d9bddf047401f24c5d30d5a1f4de79
Water <598127577@qq.com>
2016-08-28 23:06:46 +0800

    Synchronized

---
+++ TestJava/src/test5/Caller.java
@@ -0,0 +1,22 @@
+package test5;
+
+public class Caller implements Runnable {
+	String msg;
+	Callme target;
+	Thread t;
+	
+	public Caller(Callme targ, String s) {
+		this.target = targ;
+		this.msg = s;
+		t = new Thread(this);
+		t.start();
+	}
+	
+	@Override
+	public void run() {
+		target.call(msg);
+	}
+
+	
+
+}
