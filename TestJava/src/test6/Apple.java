ff31466c7f4e12f5e3e60631dc1ed8abbf6aba0d
Water <598127577@qq.com>
2016-08-30 20:35:29 +0800

    Enum

--- TestJava/src/test6/Apple.java
+++ TestJava/src/test6/Apple.java
@@ -1,5 +1,15 @@
 package test6;
 
 public enum Apple {
-	Jonathan, GoldenDel, RedDel, Winesap, Cortland
+	Jonathan(10), GoldenDel(9), RedDel(12), Winesap(15), Cortland(8);
+	
+	private int price;
+	
+	Apple(int p){
+		this.price = p;
+	}
+	
+	public int getPrice(){
+		return this.price;
+	}
 }
--- TestJava/src/test6/EnumDemo2.java
+++ TestJava/src/test6/EnumDemo2.java
@@ -3,13 +3,13 @@ package test6;
 public class EnumDemo2 {
 	public static void main(String[] args) {
 		Apple apple;
-		System.out.println("All apple constants");
+		System.out.println("All apple prices");
 		for (Apple a: Apple.values()) {
-			System.out.println(a);
+			System.out.println(a + " " + a.getPrice());
 		}
 		System.out.println();
 		
 		apple = Apple.valueOf("Winesap");
-		System.out.println("apple contains " + apple);
+		System.out.println("Winesap price " + apple.getPrice());
 	}
 }
