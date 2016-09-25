7ed36228d40cdab4b7ee2d0db87430b303deab35
WanZ <598127577@qq.com>
2016-09-25 23:11:43 +0800

    。。。

--- TestJava/src/test9/JavaFXSkel.java
+++ TestJava/src/test9/JavaFXSkel.java
@@ -1,39 +1,18 @@
 package test9;
 
 import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.layout.FlowPane;
 import javafx.stage.Stage;
 
-public class JavaFXSkel extends Application {
+public class JavaFXSkel extends Application{
 	
 	public static void main(String[] args) {
-		System.out.println("Launching JavaFX application.");
 		
-		launch();
-	}
-	
-	public void init() {
-		System.out.println("Inside the init() method.");
 	}
 
 	@Override
-	public void start(Stage myStage) {
-		System.out.println("Inside the start() method.");
+	public void start(Stage arg0) throws Exception {
+		// TODO Auto-generated method stub
 		
-		myStage.setTitle("JavaFX Skeleton.");
-		
-		FlowPane rootNode = new FlowPane();
-		
-		Scene myScene = new Scene(rootNode, 500, 400);
-		
-		myStage.setScene(myScene);
-		myStage.show();
-		
-	}
-	
-	public void stop(){
-		System.out.println("Inside the stop() method.");
 	}
 
 }
