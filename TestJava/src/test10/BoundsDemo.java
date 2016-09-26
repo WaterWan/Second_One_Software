19b863a751a85765ce2624af4fb245db89cf0552
WanZ <598127577@qq.com>
2016-09-26 23:28:13 +0800

    范型

---
+++ TestJava/src/test10/BoundsDemo.java
@@ -0,0 +1,17 @@
+package test10;
+
+public class BoundsDemo {
+
+	public static void main(String[] args) {
+		Integer inums[] = { 1, 2, 3, 4, 5 };
+		Stats<Integer> iob = new Stats<>(inums);
+		double v = iob.average();
+		System.out.println("iob value is " + v);
+		
+		Double dnums[] = { 1.1, 2.2, 3.3, 4.4, 5.5 };
+		Stats<Double> dob = new Stats<>(dnums);
+		double w = dob.average();
+		System.out.println("dob value is " + w);
+	}
+
+}
