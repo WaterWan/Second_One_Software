fa36d2b6eddaaf5d584e489df78f84fee92023cb
WanZ <598127577@qq.com>
2016-10-10 23:53:52 +0800

    print

--- TestJava/src/test13/PrintShape.java
+++ TestJava/src/test13/PrintShape.java
@@ -2,6 +2,15 @@ package test13;
 
 public class PrintShape {
 	
+	public static void main(String[] args) {
+		
+		for (int i = 1; i <= 6; i++) {
+			System.out.println(i);
+			printRhombus(i);
+			System.out.println();
+		}
+	}
+	
 	public static void printStars(int num) {
 		for (int i = 0; i < num; i++) {
 			System.out.print("*");
@@ -15,11 +24,15 @@ public class PrintShape {
 	}
 	
 	public static void printRhombus(int num) {
-		for (int i = num; i > 0; i--) {
-			printSpace(i - 1);
-			printStars(2 * (num - i) + 1);
-			System.out.println();
+		if(num % 2 == 1) {
+			printOddRhombus(num / 2 + 1);
+		}else {
+			printEvenRhombus(num / 2);
 		}
+	}
+	
+	public static void printOddRhombus(int num) {
+		printPositiveTriangle(num);
 		for (int i = 2; i <= num; i++) {
 			printSpace(i - 1);
 			printStars(2 * (num - i) + 1);
@@ -27,7 +40,21 @@ public class PrintShape {
 		}
 	}
 	
-	public static void main(String[] args) {
-		printRhombus(3);
+	public static void printEvenRhombus(int num) {
+		printPositiveTriangle(num);
+		for (int i = 1; i <= num; i++) {
+			printSpace(i - 1);
+			printStars(2 * (num - i) + 1);
+			System.out.println();
+		}
 	}
+	
+	public static void printPositiveTriangle(int num) {
+		for (int i = num; i > 0; i--) {
+			printSpace(i - 1);
+			printStars(2 * (num - i) + 1);
+			System.out.println();
+		}
+	}
+	
 }
