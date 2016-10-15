56b3105670a0eb5f4a28aa4cf76918f82ea52df4
WanZ <598127577@qq.com>
2016-10-16 00:28:28 +0800

    下一点东西

--- TestJava/src/test1/PhotoDownload.java
+++ TestJava/src/test1/PhotoDownload.java
@@ -43,7 +43,7 @@ public class PhotoDownload {
 //				"161240009",
 //		};
 //		downloadPhotos(newStudents, "E:\\图片及音频\\扬州中学16级");
-		downloadPhotos(generateIds("161220"), "E:\\图片及音频\\计科16");
+		downloadPhotos(generateIds("151150"), "E:\\图片及音频\\地科15");
 	}
 
 	public static void downloadPhotos(String[] ids, String path) throws Exception {
