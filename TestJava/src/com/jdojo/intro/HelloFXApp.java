65aaf949c18e093065adfd96b798b5a28d4f4f33
WanZ <598127577@qq.com>
2016-10-29 15:52:33 +0800

    没有main方法也可以执行的JavaFX

---
+++ TestJava/src/com/jdojo/intro/HelloFXApp.java
@@ -0,0 +1,17 @@
+package com.jdojo.intro;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class HelloFXApp extends Application {
+
+	@Override
+	public void start(Stage stage){
+		stage.setTitle("Hello JavaFX Application233");
+		stage.show();
+	}
+	
+//	public static void main(String[] args) {
+//		Application.launch(args);
+//	}
+}
