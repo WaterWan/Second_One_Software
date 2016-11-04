98507145ce510b7504c2010bafb590da2b57142b
WanZ <598127577@qq.com>
2016-11-04 23:22:14 +0800

    你什么都看不到

---
+++ TestJava/src/com/jdojo/stage/EverRunningApp.java
@@ -0,0 +1,17 @@
+package com.jdojo.stage;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class EverRunningApp extends Application {
+
+	@Override
+	public void start(Stage stage) {
+
+	}
+
+	public static void main(String[] args) {
+		Application.launch();
+	}
+
+}
