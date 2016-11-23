865384844c1e0df19a43d78514100b3794fe50d8
WanZ <598127577@qq.com>
2016-11-23 21:55:53 +0800

    一个有趣的现象产生了

---
+++ TestJava/src/test17/CacheTest.java
@@ -0,0 +1,19 @@
+package test17;
+
+import java.lang.reflect.Field;
+
+public class CacheTest {
+	public static void main(String[] args) throws NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException {
+		Class cache = Integer.class.getDeclaredClasses()[0];
+		Field myCache = cache.getDeclaredField("cache");
+		myCache.setAccessible(true);
+		
+		Integer[] newCache = (Integer[]) myCache.get(cache);
+		newCache[132] = newCache[135];
+		
+		int a = 2;
+		int b = a + a;
+		System.out.printf("%d + %d = %d", a, a, b);
+		
+	}
+}
